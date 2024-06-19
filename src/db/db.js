95b3c5542be1d35c7f5a6a1559188fb672b2c418
KevinLap <kevinzaok@outlook.com>
2024-06-18 21:07:37 -0600

    Changed Config

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,16 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC_YUgaivMq-qODVETZGA-mDwqZA1eLCts",
-//   authDomain: "animedb-project.firebaseapp.com",
-//   projectId: "animedb-project",
-//   storageBucket: "animedb-project.appspot.com",
-//   messagingSenderId: "382361713960",
-//   appId: "1:382361713960:web:edcf09c124674f5f54641d",
-//   measurementId: "G-VBE3TNS6PR"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC_YUgaivMq-qODVETZGA-mDwqZA1eLCts",
+  authDomain: "animedb-project.firebaseapp.com",
+  projectId: "animedb-project",
+  storageBucket: "animedb-project.appspot.com",
+  messagingSenderId: "382361713960",
+  appId: "1:382361713960:web:edcf09c124674f5f54641d",
+   measurementId: "G-VBE3TNS6PR"
+};
 
 
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
